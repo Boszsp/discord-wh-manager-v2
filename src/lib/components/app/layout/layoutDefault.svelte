6f98b56cf7a6eef825baafe7448d54acd02e653a
bosz-in-cloud-shell <95701554+Boszsp@users.noreply.github.com>
2025-10-03 21:04:26 +0000

    feat:update header

--- src/lib/components/app/layout/layoutDefault.svelte
+++ src/lib/components/app/layout/layoutDefault.svelte
@@ -1,43 +1,55 @@
 <script lang="ts">
-	import { HouseIcon, LogInIcon } from 'lucide-svelte';
+	import { HouseIcon, LogInIcon, ServerIcon } from 'lucide-svelte';
 	import Sidebar, { type sidebarMenuProps } from '$lib/components/app/nav/sidebar.svelte';
 	import { page } from '$app/state';
+	import { Badge } from '$lib/components/ui/badge';
+	import { APP_NAME } from '$lib/default';
 
 	let { children } = $props();
-	const datas:sidebarMenuProps[] = $derived([
+	const datas: sidebarMenuProps[] = $derived([
 		{
 			fallback: 'xx',
 			title: 'bg',
-			link:'/channel?id=1',
-			isSelected: page.url.searchParams.get("id") === "1"
+			link: '/channel?id=1',
+			color: 'red',
+			isSelected: page.url.searchParams.get('id') === '1'
 		},
 		{
 			fallback: 'xx22',
 			title: 'bg22',
-			link:'/channel?id=2',
-			isSelected: page.url.searchParams.get("id") === "2"
+			link: '/channel?id=2',
+			isSelected: page.url.searchParams.get('id') === '2'
 		}
-	])
+	]);
+	const selected = $derived(datas.filter((v) => v.isSelected).pop());
 </script>
+
 <div class="h-dvh w-dvw overflow-hidden bg-theme-accent">
-<header class="flex items-center-safe justify-between px-8 py-2">
-	<span></span>
-	<span>
-		<a href="/" class="flex items-center justify-self-center-safe">
-			<HouseIcon class="mr-2 size-4" />
-			<small class="text-sm leading-none font-medium">Home</small>
-		</a>
-	</span>
-	<span>
-		<a href="/login">
-			<LogInIcon class="size-4" />
-		</a>
-	</span>
-</header>
-<div class="flex">
-	<Sidebar sidebarMenu={datas} />
-	<main class="w-full h-[96dvh] border-t border-l rounded-tl-2xl">
-		{@render children?.()}
-	</main>
-</div>
+	<header class="flex items-center-safe justify-between px-8 py-2">
+		<span></span>
+		<span>
+			<a href="/" class="flex items-center justify-self-center-safe">
+				{#if selected}
+					<ServerIcon class="mr-2 size-4" />
+					<small class="text-sm leading-none font-medium">
+						{selected.title}
+					</small>
+				{:else}
+					<img src="/logo.png" alt="logo" class="mr-2 size-4" />
+					<small class="text-sm leading-none font-medium">{APP_NAME}</small>
+				{/if}
+			</a>
+		</span>
+		<span>
+			<a href="/login">
+				<LogInIcon class="size-4" />
+			</a>
+		</span>
+	</header>
+	<div class="flex">
+		<Sidebar sidebarMenu={datas} />
+		<main class="h-[96dvh] w-full rounded-tl-2xl border-t border-l">
+			{@render children?.()}
+		</main>
+	</div>
 </div>
