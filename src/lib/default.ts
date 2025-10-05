2e9695ebb0b6248837de1af4eafc71928eb86083
bosz-in-cloud-shell <95701554+Boszsp@users.noreply.github.com>
2025-10-05 14:14:12 +0000

    feat:update avatar image

--- src/lib/default.ts
+++ src/lib/default.ts
@@ -1,7 +1,7 @@
 import type { sidebarMenuProps } from "$lib/components/app/nav/sidebar.svelte";
 
 export const DEFAULT_SIDEBAR_FIRST_MENU:sidebarMenuProps = {
-    image: '/logo-bg.png',
+    image: '/avatar/b.png',
     fallback: 'DWM',
     title: 'Dashboard',
     link: '/',
