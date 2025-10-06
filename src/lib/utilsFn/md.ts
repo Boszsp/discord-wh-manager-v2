f8b4b42df16e07ccccd77ca46b83f33fa6d97edc
bosz-in-cloud-shell <95701554+Boszsp@users.noreply.github.com>
2025-10-06 03:19:41 +0000

    feat:add code highlight preview

--- src/lib/utilsFn/md.ts
+++ src/lib/utilsFn/md.ts
@@ -1,6 +1,33 @@
-import { marked } from 'marked';
+import { marked, type Token, type TokensList } from 'marked';
 import DOMPurify from 'isomorphic-dompurify';
+import markedShiki from 'marked-shiki'
+import { codeToHtml } from 'shiki'
 export async function convertMdToHTML(str: string) {
+    function processAllTokens(tokens: TokensList | Token[]): TokensList | Token[] {
+        console.log(tokens)
+        return tokens.map((v) => {
+            if (v.type === "heading" && v.raw.startsWith("####")) {
+                v.type = "paragraph"
+                v.text = v.raw
+                if (v.tokens?.[0] && v.tokens?.[0])
+                    (v.tokens[0] as any).text = v.text
+            }
+            console.log(v)
+            return v
+        })
+    }
+    marked.use({ hooks: { processAllTokens } })
+    marked.use(
+        markedShiki({
+            async highlight(code, lang) {
+                return await codeToHtml(code, {
+                    lang, theme: 'github-dark-default' })
+            },
+            container: `<figure class="highlighted-code">%s</figure>`}
+        )
+    )
+
     const clean = DOMPurify.sanitize((await marked(str)))
+
     return clean
 }
