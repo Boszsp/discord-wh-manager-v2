391af2573b72ce19058115d45735e2f5278cf57e
The BoszSpace <95701554+Boszsp@users.noreply.github.com>
2025-10-07 16:01:00 +0000

    fix:schema

--- src/lib/schema/webhookContentSchema.ts
+++ src/lib/schema/webhookContentSchema.ts
@@ -19,20 +19,23 @@ export const authorSchema = z.object({
 export const fieldsSchema = z.object({
     name: z.string().max(256, "Embed field name length must be less than 256 characters"),
     value: z.string().max(1024, "Embed field value length must be less than 1024 characters"),
-    inline: z.boolean(),
+    inline: z.boolean().optional(),
 });
 
 export const embedsSchema = z.object({
-    avatar_url: z.string().trim().url("Please enter a valid image URL for the avatar"),
+    avatar_url: z.string().trim().url("Please enter a valid image URL for the avatar").optional(),
     color: z.number(),
-    author: authorSchema,
+    author: authorSchema.optional(),
     title: z.string().max(256, "Title length must be less than 256 characters"),
     description: z.string().max(4096, "Description length must be less than 4096 characters"),
-    image: z.object({ url: z.string().url("Please enter a valid image URL for the embed image") }),
-    fields: fieldsSchema.array().max(25, "Fields length must be less than 25 fields per embed"),
-    footer: z.object({ text: z.string().max(2048, "Footer text length must be less than 2048 characters"), icon_url: z.string().url("Please enter a valid image URL for the footer embed icon") }),
-    thumbnail: z.object({ url: z.string().url("Please enter a valid image URL for the embed thumbnail") }),
-    url: z.string().trim().url("Please enter a valid  URL for the embed url"),
+    image: z.object({ url: z.string().url("Please enter a valid image URL for the embed image") }).optional(),
+    fields: fieldsSchema.array().max(25, "Fields length must be less than 25 fields per embed").optional(),
+    footer: z.object({
+        text: z.string().max(2048, "Footer text length must be less than 2048 characters").optional(),
+        icon_url: z.string().url("Please enter a valid image URL for the footer embed icon")
+    }).optional(),
+    thumbnail: z.object({ url: z.string().url("Please enter a valid image URL for the embed thumbnail") }).optional(),
+    url: z.string().trim().url("Please enter a valid  URL for the embed url").optional(),
 });
 
 export const hookJsonSchema = z
@@ -45,15 +48,25 @@ export const hookJsonSchema = z
         thread_name: z.string().max(100, "Thread name length must be less than 100 characters"),
     })
 
-export const hookJsonPartial = hookJsonSchema.partial()
+    
+
+export const hookJsonPartial = z
+    .object({
+        username: z.string().trim().max(80, "Username length must be less than 80 characters").optional(),
+        avatar_url: z.string().url("Please enter a valid image URL for the profile avatar").optional(),
+        content: z.string().max(2000, "Content length must be less than 2,000 characters").optional(),
+        embeds: embedsSchema.array().max(10, "Embeds length must be less than 10 items").optional(), //.max(10, "Embeds length must be less than 10 items")
+        files: filesSchema.optional(),
+        thread_name: z.string().max(100, "Thread name length must be less than 100 characters").optional(),
+    }).partial()
 export const embedsPartial = embedsSchema.partial()
 
-export type hookJsonSchemaType = z.infer<typeof hookJsonSchema> 
-export type hookJsonPartialSchemaType = z.infer<typeof hookJsonPartial> 
-export type embedsPartialSchemaType = z.infer<typeof embedsPartial> 
-export type embedsSchemaType = z.infer<typeof embedsSchema> 
-export type fieldsSchemaType = z.infer<typeof fieldsSchema> 
-export type authorSchemaType = z.infer<typeof authorSchema> 
-export type filesSchemaType = z.infer<typeof filesSchema> 
-export type fileSchemaType = z.infer<typeof fileSchema> 
+export type hookJsonSchemaType = z.infer<typeof hookJsonSchema>
+export type hookJsonPartialSchemaType = z.infer<typeof hookJsonPartial>
+export type embedsPartialSchemaType = z.infer<typeof embedsPartial>
+export type embedsSchemaType = z.infer<typeof embedsSchema>
+export type fieldsSchemaType = z.infer<typeof fieldsSchema>
+export type authorSchemaType = z.infer<typeof authorSchema>
+export type filesSchemaType = z.infer<typeof filesSchema>
+export type fileSchemaType = z.infer<typeof fileSchema>
 export type urlSchemaType = z.infer<typeof urlSchema>
