9261a4c0740f5ac50df01c7d1e8fb96ea8edb774
bosz-in-cloud-shell <95701554+Boszsp@users.noreply.github.com>
2025-10-15 15:21:33 +0000

    feat:update manipulation file ui

--- src/lib/components/app/file/file-manipulation.svelte
+++ src/lib/components/app/file/file-manipulation.svelte
@@ -1,10 +1,15 @@
 <script lang="ts" module>
 	import { z } from 'zod/v4';
+	import { MAX_FILE_SIZE } from '$lib/default';
 
 	const formSchema = z.object({
 		quality: z.number().min(0).max(100).default(100),
 		scale: z.number().min(0).max(1).default(1),
-		extension: z.enum(['png', 'jpg', 'webp']).default('jpg')
+		extension: z.enum(['png', 'jpg', 'webp']).default('jpg'),
+		fileSizeLimit: z.number().min(1).default(MAX_FILE_SIZE/1024/1024),
+		fileName: z.string().trim(),
+		isRemoveSoure: z.boolean().default(false),
+		isFixedSize: z.boolean().default(false),
 	});
 </script>
 
@@ -26,6 +31,16 @@
 	import { zod4 } from 'sveltekit-superforms/adapters';
 	import { toast } from 'svelte-sonner';
 	import * as Form from '$lib/components/ui/form/index.js';
+	import { Separator } from '$lib/components/ui/separator';
+	import {
+		FileTextIcon,
+		ImageIcon,
+		MinusIcon,
+		PackageIcon,
+		PackageOpen,
+		SquareBottomDashedScissorsIcon
+	} from 'lucide-svelte';
+	import { Toggle } from '$lib/components/ui/toggle';
 
 	const form = superForm(defaults(zod4(formSchema)), {
 		validators: zod4(formSchema),
@@ -50,43 +65,68 @@
 		<CardDescription>Manipulation file</CardDescription>
 	</CardHeader>
 	<CardContent class="grid gap-4">
-		<div class="grid grid-cols-2 gap-2">
-			<Button variant="outline">Zip</Button>
-			<Button variant="outline">Unzip</Button>
-			<Button variant="outline">Image to PDF</Button>
-			<Button variant="outline">PDF to Image</Button>
-			<Button variant="outline">Split PDF</Button>
+		<Form.Field {form} name="fileName" >
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Name</Form.Label>
+						<Input {...props} bind:value={$formData.fileName} />
+					{/snippet}
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+		<div class="flex gap-2">
+			
+			<Form.Field {form} name="fileSizeLimit">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Maximum Limit Size</Form.Label>
+						<Input type="number" {...props} bind:value={$formData.fileSizeLimit} />
+					{/snippet}
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<div>
+				<Label class="mb-2 opacity-0">Rem Source</Label>
+			<Toggle variant="outline" >
+				<MinusIcon />Remove source</Toggle
+			></div>
+			<div>
+				<Label class="mb-2 opacity-0">Fixed size</Label>
+			<Toggle variant="outline">
+				<MinusIcon />Fixed pdf page size</Toggle
+			></div>
 		</div>
+		<div class="grid grid-cols-3 gap-2">
+			<Button variant="outline"><PackageIcon /> Zip</Button>
+			<Button variant="outline"><PackageOpen /> Unzip</Button>
+			<Button variant="outline"><SquareBottomDashedScissorsIcon /> Split Zip</Button>
+		</div>
+		<div class="grid grid-cols-3 gap-2">
+			<Button variant="outline"><FileTextIcon /> Image to PDF</Button>
+			<Button variant="outline"><ImageIcon /> PDF to Image</Button>
+			<Button variant="outline"><SquareBottomDashedScissorsIcon /> Split PDF</Button>
+		</div>
+		<Separator class="my-4" />
 		<div class="grid gap-2">
-			<CardTitle>Change Image Extension</CardTitle>
-			<CardDescription>Manipulation file</CardDescription>
-			<div class="grid grid-cols-2 gap-2">
-				<div class="grid gap-1">
-					<Form.Field {form} name="quality">
-						<Form.Control>
-							{#snippet children({ props })}
-								<Form.Label>Quality</Form.Label>
-								<Input type="number" {...props} bind:value={$formData.quality} />
-							{/snippet}
-						</Form.Control>
-						<Form.Description>accept 0-100</Form.Description>
-						<Form.FieldErrors />
-					</Form.Field>
-				</div>
-				<div class="grid gap-1">
-					<Form.Field {form} name="scale">
-						<Form.Control>
-							{#snippet children({ props })}
-								<Form.Label>Scale</Form.Label>
-								<Input step="0.1" type="number" {...props} bind:value={$formData.scale} />
-							{/snippet}
-						</Form.Control>
-						<Form.Description>accept float 0-1</Form.Description>
-						<Form.FieldErrors />
-					</Form.Field>
-				</div>
-			</div>
-			<div class="grid gap-1">
+			<div class="flex w-full gap-2">
+				<Form.Field {form} name="quality">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Quality</Form.Label>
+							<Input type="number" {...props} bind:value={$formData.quality} />
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field {form} name="scale">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Scale</Form.Label>
+							<Input step="0.1" type="number" {...props} bind:value={$formData.scale} />
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
 				<Form.Field {form} name="extension">
 					<Form.Control>
 						{#snippet children({ props })}
@@ -96,20 +136,20 @@
 									{$formData.extension ? $formData.extension : 'Select a verified extension'}
 								</Select.Trigger>
 								<Select.Content>
-									<Select.Item value="png">png</Select.Item>
 									<Select.Item value="jpg">jpg</Select.Item>
+									<Select.Item value="png">png</Select.Item>
 									<Select.Item value="webp">webp</Select.Item>
 								</Select.Content>
 							</Select.Root>
 						{/snippet}
 					</Form.Control>
-					<Form.Description>
-						Selected output extension
-					</Form.Description>
 					<Form.FieldErrors />
 				</Form.Field>
+				<div class="ml-auto">
+					<Label class="mb-2 opacity-0">Button</Label>
+					<Button variant="outline">Change Extension</Button>
+				</div>
 			</div>
-			<Button>Change Extension</Button>
 		</div>
 	</CardContent>
 </Card>
