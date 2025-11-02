02ac56fc5f070ddd3bf0625cd8dfa8e08b474c58
bosz-in-cloud-shell <95701554+Boszsp@users.noreply.github.com>
2025-11-02 18:03:30 +0000

    fix:redirect fn 2

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -22,7 +22,7 @@ export const load: LayoutLoad = async ({ depends, url }) => {
 		//if (public_paths.filter(v => !['/', '', '/default', '/default/', '/home', '/home/'].includes(v)).includes(pathname))
 		//	return {}
 		
-		//consola.info(pathname)
+		consola.info(pathname)
 
 		let user = await getCurUserPromise().catch()
 		//consola.info('Loading user', user);
@@ -39,7 +39,7 @@ export const load: LayoutLoad = async ({ depends, url }) => {
 
 		if((!user) && pathname.startsWith("/login"))return
 
-		if (!(user && public_paths.includes(pathname))) {
+		if (!(user || public_paths.includes(pathname))) {
 			throw redirect(302, '/login');
 		}
 
