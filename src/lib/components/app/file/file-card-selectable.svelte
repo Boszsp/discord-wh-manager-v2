39a8afa296328bd9835bbf8c15f0c8a0c7b5de2a
bosz-in-cloud-shell <95701554+Boszsp@users.noreply.github.com>
2025-11-03 05:37:53 +0000

    feat:preview file names in zip file

--- src/lib/components/app/file/file-card-selectable.svelte
+++ src/lib/components/app/file/file-card-selectable.svelte
@@ -4,19 +4,26 @@
 	import type { ClassValue } from 'svelte/elements';
 	import { selectedFileStore } from '../../../store/selected-file-store.svelte';
 	import { cn } from '$lib/utils';
+	import { Button } from '$lib/components/ui/button';
+	import { zipMimeTypeList } from '$lib/utilsFn/zip';
+	import ZipPreviewDialog from './zip-preview-dialog.svelte';
+	import type { FileType } from '../types';
 
 	let {
 		id,
 		title,
 		description,
+		file,
 		class: className
 	}: {
 		title: string;
 		description: string;
 		id: string;
+		file: FileType;
 		class?: ClassValue;
 	} = $props();
 	let count = $state(0);
+	let showPreview = $state(false);
 
 	function clickHandler() {
 		if (count === 0) {
@@ -33,31 +40,57 @@
 	}
 </script>
 
+{#if zipMimeTypeList.includes(file.file.type)}
+	<ZipPreviewDialog bind:open={showPreview} file={file.file} />
+{/if}
 {#key $selectedFileStore}
-	<Label
+	<div
 		class={cn(
-			'flex items-start gap-3 rounded-lg border  p-3 hover:bg-accent/50 has-[[aria-checked=true]]:border-indigo-600',
-			'has-[[aria-checked=true]]:bg-indigo-50 dark:has-[[aria-checked=true]]:border-indigo-900 dark:has-[[aria-checked=true]]:bg-indigo-950',
-			'has-[[aria-checked=mixed]]:border-indigo-600 has-[[aria-checked=mixed]]:bg-indigo-50 dark:has-[[aria-checked=mixed]]:border-indigo-900 dark:has-[[aria-checked=mixed]]:bg-indigo-950',
-
+			'flex items-start gap-3 rounded-lg border p-3 hover:bg-accent/50 has-[:checked]:border-indigo-600',
+			'has-[:checked]:bg-indigo-50 dark:has-[:checked]:border-indigo-900 dark:has-[:checked]:bg-indigo-950',
 			className
 		)}
-		aria-label={id}
-		onclick={clickHandler}
 	>
-		<Checkbox
-			id="toggle-2"
-			checked={$selectedFileStore.includes(id)}
-			value={id}
-			class="data-[state=checked]:border-secondary data-[state=checked]:bg-indigo-600 data-[state=checked]:text-white dark:data-[state=checked]:border-indigo-700 dark:data-[state=checked]:bg-indigo-700"
-		/>
-		<div class="grid gap-1.5 font-normal">
-			<p class="text-sm leading-none font-medium">
-				{title?.slice(0, 48)}{title?.length > 48 ? '...' : ''}.{title?.split('.')?.pop()}
-			</p>
-			<p class="text-xs text-muted-foreground">
-				{description}
-			</p>
-		</div>
-	</Label>
+		<Label class="flex items-center gap-3" aria-label={id} onclick={clickHandler}>
+			<Checkbox
+				id="toggle-2"
+				checked={$selectedFileStore.includes(id)}
+				value={id}
+				class="data-[state=checked]:border-secondary data-[state=checked]:bg-indigo-600 data-[state=checked]:text-white dark:data-[state=checked]:border-indigo-700 dark:data-[state=checked]:bg-indigo-700"
+			/>
+			<div class="grid gap-1.5 font-normal">
+				<p class="text-sm leading-none font-medium">
+					{title?.slice(0, 48)}{title?.length > 48 ? '...' : ''}.{title?.split('.')?.pop()}
+				</p>
+				<p class="text-xs text-muted-foreground">{description}</p>
+			</div>
+		</Label>
+		{#if zipMimeTypeList.includes(file.file.type)}
+			<Button
+				size="icon"
+				variant="ghost"
+				class="ml-auto h-8 w-8"
+				onclick={() => (showPreview = true)}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-file-archive"
+					><path d="M4 22V4c0-1.1.9-2 2-2h8.5L20 7.5V20c0-1.1-.9-2-2-2H6c-1.1 0-2 .9-2 2z" /><path
+						d="M14 2v6h6"
+					/><path d="M10 12.5v-1" /><path d="M10 17.5v-1" /><path d="M10 15h-1" /><path
+						d="M10 15h1"
+					/></svg
+				>
+			</Button>
+		{/if}
+	</div>
 {/key}
+
